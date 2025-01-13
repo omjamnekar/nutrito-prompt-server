ac0de70db557dbcaa3fb5e10731c0f1732a66183
omjamnekar <omjamnekar877@gmail.com>
2025-01-13 20:22:33 +0530

    nutrito server maintaining

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
 import sys
 import os
-import jwt
-from PIL import Image
 from io import BytesIO
 
 import datetime
 from functools import wraps
 from flask import Flask, request, jsonify
 from PIL import Image
-
-
-
-
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
 
@@ -23,34 +17,6 @@ from src.routes.rest_urls import Prompt_Url
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.getenv("APP_CONFIG_KEY")
-
-
-from flask import request, jsonify
-from PIL import Image
-
-
-
-
-
-def token_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = request.headers.get('Authorization')
-        if not token:
-            return jsonify({'message': ErrorForms.token_missing}), ErrorNumber.forbidden
-
-        try:
-            token = token.replace("Bearer ", "")
-            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
-            current_user = data['user']
-        except jwt.ExpiredSignatureError:
-            return jsonify({'message': 'Token has expired!'}), ErrorNumber.forbidden
-        except jwt.InvalidTokenError:
-            return jsonify({'message': 'Token is invalid!'}), ErrorNumber.forbidden
-
-        return f(current_user, *args, **kwargs)
-    
-    return decorated
 
 
 
@@ -65,32 +31,12 @@ def onLoad():
 
 
 
-@app.route(Prompt_Url.login, methods=['POST'])
-def login():
-    auth =request.json
-    if not auth or not auth.get('username') or not auth.get('password'):
-        return jsonify({'message': ErrorForms.could_not_verify, 'WWW-Authenticate': ErrorForms.login_required}), ErrorNumber.unauthorized
-
-    _username = auth.get('username')
-    _password = auth.get('password')
-   
-    if validate_user(_username, _password):
-        
-        token = jwt.encode({
-            'user': _username,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
-        }, app.config['SECRET_KEY'], algorithm="HS256")
-        return jsonify({'token': token})
-    
-    return jsonify({'message': ErrorForms.envalid_credential}), ErrorNumber.unauthorized
-
-
 #  //nutrition 
 #/////////////////////////////////////////////////////////////////
 
 @app.route(Prompt_Url.initial_prompt, methods=['POST'])
-@token_required
-def upload_initial_image(current_user):
+# @token_required
+def upload_initial_image():
     
     try:
            
@@ -115,8 +61,8 @@ def upload_initial_image(current_user):
 
 
 @app.route(Prompt_Url.ratio_prompt, methods=['POST'])
-@token_required
-def upload_ratio_image(current_user):
+# @token_required
+def upload_ratio_image():
     try:
         if 'image' not in request.files:
             return jsonify({"error": "No image file found in the request"}), 400
@@ -138,8 +84,8 @@ def upload_ratio_image(current_user):
 
 
 @app.route(Prompt_Url.health_prompt, methods=['POST'])
-@token_required
-def upload_health_image(current_user):
+# @token_required
+def upload_health_image():
     try:
         if 'image' not in request.files:
             return jsonify({"error": "No image file found in the request"}), 400
@@ -162,8 +108,8 @@ def upload_health_image(current_user):
 
 
 @app.route(Prompt_Url.conclusion_prompt, methods=['POST'])
-@token_required
-def upload_conclusion_image(current_user):
+# @token_required
+def upload_conclusion_image():
     try:
         if 'image' not in request.files:
             return jsonify({"error": "No image file found in the request"}), 400
